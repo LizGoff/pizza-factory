aaac5d3c71ed4df811bbb6be45c8e3ca9eba68d1
Liz Goff <livingearthcreations@gmail.com>
2018-05-15 11:31:35 -0500

    commit often

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello World!')
+
+class Pizza {
+    constructor( type, price, ingredients ) {
+        this.type = type;
+        this.price = 10;
+        this.ingredients = ingredients;
+    }
+}
